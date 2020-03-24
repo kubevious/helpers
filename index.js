3b785f5823ecca72293d132f732905c7f84e7ca3
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-03-24 13:56:00 -0700

    Added event dampener

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
     DateUtils: require("./lib/date-utils"),
     DnUtils: require("./lib/dn-utils"),
     MySqlDriver: require("./lib/mysql-driver"),
+    EventDampener: require("./lib/event-dampener"),
 }
