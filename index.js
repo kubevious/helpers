5ee02738fa95fc7f9bb7e415f70264187945415b
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-03-25 15:10:48 -0700

    added buffer utils

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     DateUtils: require("./lib/date-utils"),
+    BufferUtils: require("./lib/buffer-utils"),
     DnUtils: require("./lib/dn-utils"),
     MySqlDriver: require("./lib/mysql-driver"),
     EventDampener: require("./lib/event-dampener"),
