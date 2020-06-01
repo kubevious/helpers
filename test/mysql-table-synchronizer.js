6ae61ddbd724b35cf7c52a1daa9989f8d7e22c1a
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-06-01 13:48:03 -0700

    fixes to mysql driver statement prepare

--- test/mysql-table-synchronizer.js
+++ test/mysql-table-synchronizer.js
@@ -17,8 +17,7 @@ describe('mysql-table-synchronizer', function() {
     process.env.MYSQL_USER = 'root';
     process.env.MYSQL_PASS = '';
 
-
-    it('execute', function() {
+    it('execute-after-connect', function() {
         var mysqlDriver = new MySqlDriver(logger, true);
 
         mysqlDriver.onMigrate(() => {
@@ -33,7 +32,54 @@ describe('mysql-table-synchronizer', function() {
 
         mysqlDriver.connect();
 
+        return new Promise((resolve, reject) => {
+            mysqlDriver.onConnect(() => {
+                resolve();
+            })
+        })
+        .then(() => {
+            var synchronizer = new MySqlTableSynchronizer(logger, mysqlDriver, 'sync_test', [], ['name', 'msg']);
+            return synchronizer.execute({}, [
+                { name: 'dog', msg: 'hello'}, 
+                { name: 'dog', msg: 'wof-wof'}, 
+                { name: 'cat', msg: 'hi'}, 
+                { name: 'cat', msg: 'meau'},
+            ])
+        })
+        .then(() => {
+            return mysqlDriver.executeSql("SELECT * FROM `sync_test`;");
+        })
+        .then(result => {
+            // console.log(result);
+            // (result).should.be.an.Array();
+            // (result.length > 1).should.be.true();
+        })
+        .then(() => {
+            return mysqlDriver.executeSql("DROP TABLE `sync_test`;");
+        })
+        .then(() => {
+            return mysqlDriver.close();
+        });
+    });
+
+
+    it('execute-before-connect', function() {
+
+        var mysqlDriver = new MySqlDriver(logger, true);
         var synchronizer = new MySqlTableSynchronizer(logger, mysqlDriver, 'sync_test', [], ['name', 'msg']);
+
+        mysqlDriver.onMigrate(() => {
+            return mysqlDriver.executeSql(
+                "CREATE TABLE IF NOT EXISTS `sync_test` (" +
+                    "`id` int unsigned NOT NULL AUTO_INCREMENT," +
+                    "`name` varchar(128) NOT NULL," +
+                    "`msg` TEXT NOT NULL," +
+                    "PRIMARY KEY (`id`)" +
+                ") ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+        })
+
+        mysqlDriver.connect();
+
 
         return new Promise((resolve, reject) => {
             mysqlDriver.onConnect(() => {
