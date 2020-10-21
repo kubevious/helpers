61b31ae67e80934bb68b3819760d59a46033242f
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-10-20 18:10:56 -0700

    added series resampler

---
+++ test/series-resampler.js
@@ -0,0 +1,65 @@
+const should = require('should');
+const _ = require('the-lodash');
+const Promise = require('the-promise');
+const SeriesResampler = require('../lib/history/series-resampler');
+
+const logger = require('the-logger').setup('test', { pretty: true }).sublogger('Tracker');
+
+describe('series-resampler', function() {
+
+    it('test-empty', function() {
+        let resampler = new SeriesResampler(10);
+        let result = resampler.process([]);
+        should(result).be.an.Array();
+        should(result.length).be.equal(0);
+    });
+
+    it('test-one', function() {
+        let resampler = new SeriesResampler(10);
+        let result = resampler.process([{ date: new Date() }]);
+        should(result).be.an.Array();
+        should(result.length).be.equal(1);
+    });
+
+
+    it('test-two', function() {
+        let resampler = new SeriesResampler(10);
+        let importData =  [
+            {"date":"2020-10-07 22:08:50", "changes":7, "error":42, "warn":368},
+            {"date":"2020-10-20 23:24:32", "changes":7, "error":54, "warn":367},
+        ];
+        let result = resampler.process(importData);
+        should(result).be.an.Array();
+        should(result.length).be.equal(2);
+    });
+
+    it('test-case-01', function() {
+        let resampler = new SeriesResampler(10)
+            .column("changes", _.max)
+            .column("error", _.mean)
+            ;
+
+        let importData =  [
+            {"date":"2020-10-07 22:08:50", "changes":7, "error":42, "warn":368},
+            {"date":"2020-10-14 12:07:23", "changes":6, "error":54, "warn":370},
+            {"date":"2020-10-20 23:24:32", "changes":7, "error":54, "warn":367},
+        ];
+        for(let x of importData) {
+            x.date = new Date(x.date);
+        }
+        let result = resampler.process(importData);
+        should(result).be.an.Array();
+        should(result.length).be.equal(11);
+
+        for(let p of result)
+        {
+            should(p).be.an.Object();
+            should(p.date).be.a.ok();
+            should(p.changes).be.a.Number();
+            should(p.error).be.a.Number();
+            should(p.warn).not.be.ok();
+        }
+    });
+
+
+});
