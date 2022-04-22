dc14e4340d86e4593b840855cc890584ac6f356b
Ruben Hakopian <ruben.hakopian@gmail.com>
2022-04-22 12:09:15 -0700

    moved types to agent-middleware module

---
+++ dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REPO_DEPENDENCIES=(
+    "the-lodash"
+    "the-logger"
+    "the-promise"
+)
+
+
+FORCE_RESOLVE_DEPENDENCIES=(
+    "the-lodash"
+    "the-logger"
+    "the-promise"
+)
